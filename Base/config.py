ab2bfbb710c994df4f17503d6e37aa7f760e5927
Karthigam5 <Karthigaec@gmail.com>
2026-08-26 02:08:07 -0500

    incorporated webdriver manager

--- Base/config.py
+++ Base/config.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 
 class BaseDriver:
     def __init__(self, browser="chrome"):
         if browser == "chrome":
-            self.driver = webdriver.Chrome()
+            _service = ChromeService(ChromeDriverManager().install())
+            self.driver = webdriver.Chrome(service=_service)
         elif browser == "firefox":
-            self.driver = webdriver.Firefox()
+            _service = FirefoxService(GeckoDriverManager().install())
+            self.driver = webdriver.Firefox(service=_service)
         self.driver.maximize_window()
 
     def get_driver(self):
