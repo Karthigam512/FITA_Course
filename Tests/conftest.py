d4e721b4778204a43251781a3295dd9180f015fa
Karthigam5 <Karthigaec@gmail.com>
2026-08-30 04:32:32 -0500

    fix firefox

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -9,13 +9,13 @@ from selenium.webdriver.firefox.options import Options
 
 @pytest.fixture(params=["chrome", "firefox"], scope='class')
 def init_driver(request):
+    _options = Options()
+    _options.add_argument("--headless")
+    _options.add_argument("--window-size=1920,1080")
     if request.param == "chrome":
         _service = ChromeService(ChromeDriverManager().install())
-        web_driver = webdriver.Chrome(service=_service)
+        web_driver = webdriver.Chrome(service=_service, options=_options)
     elif request.param == "firefox":
-        _options = Options()
-        _options.add_argument("--headless")
-        _options.add_argument("--window-size=1920,1080")
         _service = FirefoxService(GeckoDriverManager().install())
         web_driver = webdriver.Firefox(service=_service, options=_options)
     request.cls.driver = web_driver
